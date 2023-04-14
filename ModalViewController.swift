55891c9facbb29098a1321fbab937720ff285a60
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-04-14 15:02:44 +0900

    first commit

---
+++ ModalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ModalViewController.swift
+//  ScreenTransition
+//
+//  Created by 村上真 on 2023/04/14.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
