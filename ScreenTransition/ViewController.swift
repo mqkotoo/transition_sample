cc7be8563de19dd3683c67408a3d7e9f6e53cba9
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-04-14 14:43:17 +0900

    Initial Commit

---
+++ ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by 村上真 on 2023/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
